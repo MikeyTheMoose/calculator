5283e303ff689766d78cb056b25cf4d035115351
Mikey <110850792+MikeyTheMoose@users.noreply.github.com>
2025-04-10 09:02:21 -0500

    Update basic container

--- script.js
+++ script.js
@@ -14,3 +14,8 @@ function multiply(num1, num2) {
     return num1 * num2;
 }
 
+function addButtons() {
+    const modButtons = ['AC','+/-','%'];
+    const operatorButtons = '/*-+='
+    const numberButtons = '0123456789'
+}

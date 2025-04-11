703371501d2ad7aa7fd07bec7dc2331a977eae0b
Mikey <110850792+MikeyTheMoose@users.noreply.github.com>
2025-04-10 23:46:00 -0500

    Add operator handling. Still WIP

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-let result = '0';
-let expression = "0";
+let result = 0;
+let current = '0';
+let expression = '';
 let prevType = null;
+let prevOp = null;
 let operation = null;
-
-function add(num1, num2) {
-    return num1 + num2;
-}
-
-function subtract(num1, num2) {
-    return num1 - num2;
-}
-
-function divide(num1, num2) {
-    return num1 / num2;
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
+const displayResult = document.querySelector('.result');
+const displayExpression = document.querySelector('.expression');
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach(btn => btn.addEventListener('click',buttonType))
@@ -32,48 +20,58 @@ function buttonType(btn) {
             break;
         case 'operator':
             handleOperator(selection) 
-        // handleOperator(selection.id);
             break;
         case 'special':
             handleSpecial(selection.id)
             break;
     }
-    // result = selection;
-    // expression += selection + " ";
-    // const displayResult = document.querySelector('.result');
-    // const displayExpression = document.querySelector('.expression');
-    // displayResult.textContent = result;
-    // displayExpression.textContent = expression;
 }
 
 function handleNumber(num) {
     switch (prevType) {
         case null:
             prevType = 'number';
-            result = num;
-            expression = num;
+            current = num;
             break;
         case 'number':
-            result += num;
+            current += num;
             break;
         case 'operator':
             prevType = 'operator';
     }
 
-    const displayResult = document.querySelector('.result');
-    displayResult.textContent = result;
+    
+    displayResult.textContent = current;
+    //handleExpression(num);
+    expression += num;
 
 }
 
 function handleOperator(op) {
-    prevType = 'operator';
-    operation = op.id;
 
-    if (op.id === 'equals') {
-        
-        if (prevType === 'number') {
-            handleExpression(op.textContent)
+    // Once a second operator is entered, perform the first operator.
+    // Ex: 75 x 10 x -> perform the multiplication, set 'result' equal to value.
+    if (prevOp) {
+        switch (prevOp) {
+            case 'plus':
+                add(result,current);
+                break;
+            case 'minus':
+                subtract(result,current);
+                break;
+            case 'divide':
+                divide(result,current);
+                break;
+            case 'multiply':
+                multiply(result,current);
+                break;
         }
+        
+    }
+
+    if (prevOp != 'equals') {
+        prevOp = op.id;
+        handleExpression(op.textContent);
     }
 }
 
@@ -83,4 +81,22 @@ function handleSpecial(sign) {
 
 function handleExpression(content) {
     expression += content;
+    displayExpression.textContent = expression;
+    current = ' ';
+}
+
+function add(num1, num2) {
+    result = num1 + num2;
+}
+
+function subtract(num1, num2) {
+    result = num1 - num2;
+}
+
+function divide(num1, num2) {
+    result = num1 / num2;
+}
+
+function multiply(num1, num2) {
+    result = num1 * num2;
 }

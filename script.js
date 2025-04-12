81d0f3462b01c19420b5c5c891fdbfc132ab01dc
MikeyTheMoose <mikeydermoose@gmail.com>
2025-04-12 11:46:10 -0500

    Fix functionality for Equals operation

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let current = '0';
 let expression = '';
 let prevType = null;
 let prevOp = null;
+let isEquals = false;
+let prevNumber = null;
 
 const displayResult = document.querySelector('.result');
 const displayExpression = document.querySelector('.expression');
@@ -24,65 +26,97 @@ function buttonType(btn) {
         case 'special':
             handleSpecial(selection.id)
             break;
+        case 'equals':
+            handleEquals();
+            break;
     }
 }
 
 function handleNumber(num) {
     switch (prevType) {
         case null:
-            prevType = 'number';
             current = num;
-            console.log(prevType)
             break;
         case 'number':
             current += num;
             break;
         case 'operator':
-            // ??? Maybe depends on if it's equals or not?
-            // Otherwise don't need this.
+            current = num;
+            break;
     }
 
-    expression += num;
+    prevType = 'number';
+
+    // We only want to add the number to display if an operator is pressed...
+    //expression += num;
     handleDisplay();
 }
 
 function handleOperator(op) {
+    if (prevType != 'equals') {
+        // If the most recent operator pressed was equals, then we do NOT want to perform the operation again.
+        // Otherwise 2x2=[4]+2= will yield 2x2=[4]+[8]2= [10], performing the multiplication a second time, as that is still prevOp.
 
+        // If we have not just pressed equals, then we check if an operator has already been pressed. If so, we calculate it once a new operator is pressed,
+        //     as that indicates we are no longer pressing numbers.
+
+        if (prevOp) {
+            performOperation();
+        } else {
+            result = current;
+        }
+    } else {
+        expression = '';
+    }
+    prevType = 'operator';
+    expression += current + op.textContent;
+    prevOp = op.id;
+    current = result;
+    handleDisplay();
     // Once a second operator is entered, perform the first operator.
     // Ex: 75 x 10 x -> perform the multiplication, set 'result' equal to value.
-    if (prevOp) {
-        switch (prevOp) {
-            case 'plus':
-                add(result,current);
-                break;
-            case 'minus':
-                subtract(result,current);
-                break;
-            case 'divide':
-                divide(result,current);
-                break;
-            case 'multiply':
-                multiply(result,current);
-                break;
-        }
+}
+
+function performOperation(num = current) {
+    switch (prevOp) {
+        case 'plus':
+            add(result,num);
+            break;
+        case 'minus':
+            subtract(result,num);
+            break;
+        case 'divide':
+            divide(result,num);
+            break;
+        case 'multiply':
+            multiply(result,num);
+            break;
+    }
+}
+
+function handleEquals() {
+    if (prevType === 'number') {
+        prevNumber = current;
+        console.log("test",prevNumber)
     }
 
-    if (prevOp != 'equals') {
-        prevOp = op.id;
-        handleExpression(op.textContent);
+    if (prevOp) {
+        performOperation(prevNumber);
     }
+    expression = result + prevOp + prevNumber;
+    current = result;
+    handleDisplay();
+
+    prevType = 'equals';
 }
 
 function handleSpecial(sign) {
     switch (sign) {
         case 'clear':
-            result = 0;
-            expression = '';
-            prevType = null;
-            prevOp = null;
-            current = '0';
+            reset();
+            break;
         case 'backspace':
-            current = current.substr(0,current.length-1)
+            current = current.substring(0,current.length-1)
             break;
         case 'plusminus':
             current = (+current) * -1;
@@ -93,10 +127,13 @@ function handleSpecial(sign) {
     handleDisplay();
 }
 
-function handleExpression(content) {
-    expression += content;
-    displayExpression.textContent = expression;
-    current = ' ';
+function reset() {
+    result = 0;
+    expression = '';
+    prevType = null;
+    prevOp = null;
+    current = '0';
+    isEquals = false;
 }
 
 function handleDisplay() {
@@ -105,7 +142,7 @@ function handleDisplay() {
 }
 
 function add(num1, num2) {
-    result = num1 + num2;
+    result = +num1 + +num2;
 }
 
 function subtract(num1, num2) {
